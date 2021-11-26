afc7462e5f0fdcfad4e3524526595d9488f67ac2
renatodamas <damas.renato@gmail.com>
2021-11-26 20:30:09 -0300

    Django Admin

--- wfm/settings.py
+++ wfm/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
+print(BASE_DIR)
+print(STATIC_URL)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
