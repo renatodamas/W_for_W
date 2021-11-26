dc808f51f32dfa9e8596d831a67fd70b4209e61e
renatodamas <damas.renato@gmail.com>
2021-11-26 19:17:50 -0300

    Django Admin: adicionado usuario

--- Procfile
+++ Procfile
@@ -1,5 +1,4 @@
 // Configurações do Servidor Heroku
-// release: python manage.py migrate --noinput
+release: python manage.py migrate --noinput
 web: gunicorn wfm.wsgi --log-file -
 
-//
--- wfm/settings.py
+++ wfm/settings.py
@@ -23,6 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
+# Customized User Class
+AUTH_USER_MODEL = 'base.User'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
