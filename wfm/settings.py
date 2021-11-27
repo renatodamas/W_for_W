77a8a22500988b1ad550e57a98ed8c5f146dfd88
renatodamas <damas.renato@gmail.com>
2021-11-26 23:16:10 -0300

    Django Admin heroku

--- wfm/settings.py
+++ wfm/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # Configuração de ambiente de desenvolvimento
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 COLLECTFAST_ENABLED = False
