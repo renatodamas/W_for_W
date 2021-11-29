d384672bfe74bc19d5fce2daa5b72757efc0dfcd
renatodamas <damas.renato@gmail.com>
2021-11-29 01:28:55 -0300

    Django admin

--- wfm/base/forms.py
+++ wfm/base/forms.py
@@ -6,4 +6,3 @@ from wfm.base.models import Item
 class DoacaoChangeListForm(forms.ModelForm):
     # here we only need to define the field we want to be editable
     item = forms.ModelMultipleChoiceField(queryset=Item.objects.all(), required=False)
-
